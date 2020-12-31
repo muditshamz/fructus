2c1f8dfbad0a87f00842657e147c0b6cd2770803
Mudit Sharma <muditshamz@gmail.com>
2020-12-31 21:45:05 +0530

    Initial Commit

---
+++ fructus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fructus
+//
+//  Created by Mudit Sharma on 31/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fructus/fructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  fructusApp.swift
+//  fructus
+//
+//  Created by Mudit Sharma on 31/12/20.
+//
+
+import SwiftUI
+
+@main
+struct fructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
